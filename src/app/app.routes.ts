bcd94d6fc01f96862c828861395d55fa71ad6af6
amedhue1804 <amedhue1804@iescarrillo.es>
2024-12-01 16:03:49 +0100

    arreglos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {StatsComponent} from './components/dashboard/stats/stats.component';
+import {ProfileComponent} from './components/dashboard/profile/profile.component';
+import {LoginComponent} from './pages/auth/login/login.component';
+import {SiginComponent} from './pages/auth/sigin/sigin.component';
+import {TasksComponent} from './pages/tasks/tasks.component';
+import {HomeComponent} from './pages/home/home.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {NotfoundComponent} from './pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'singin', component: SiginComponent},
+  {path: 'home', component: HomeComponent},
+  {path:'tasks', component: TasksComponent},
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      {path: 'stats', component: StatsComponent},
+      {path: 'profile', component: ProfileComponent}
+    ]
+  },
+  {path: 'notfound', component: NotfoundComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/notfound', pathMatch: 'full'},
+];
--- src/app/models/taskEvent.model.ts
+++
@@ -1,9 +0,0 @@
-export class TaskEvent{
-    action:string;
-    taskId:number;
-
-    constructor(action:string,taskId:number){
-        this.action = action;
-        this.taskId = taskId;
-    }
-}
