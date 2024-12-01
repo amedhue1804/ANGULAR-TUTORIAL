1309ae90c120090b0e1b3c4ee309d91414f1947b
amedhue1804 <amedhue1804@iescarrillo.es>
2024-12-01 16:20:50 +0100

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from "./components/auth/login/login.component";
-import { SiginComponent } from "./components/auth/sign/sign.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { TasklistComponent } from "./components/task/tasklist/tasklist.component";
 import { FormsModule } from '@angular/forms';
-import { TaskresumeComponent } from "./components/task/taskresume/resume.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FormsModule, FooterComponent, TasklistComponent],
+  imports: [RouterOutlet, NavBarComponent, FormsModule, FooterComponent, TasklistComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
